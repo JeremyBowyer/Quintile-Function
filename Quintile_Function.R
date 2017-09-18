8fba5989f4f7e5e49a36bf14068dac70f005035a
Jeremy Bowyer <JeremyBowyer@gmail.com>
2017-09-18 12:42:16 -0400

    Added parameter for descending / ascending

--- Quintile_Function.R
+++ Quintile_Function.R
@@ -1,11 +1,12 @@
 
 ## Function that transforms numeric vector into quintiles
-quint <- function(x) {
+quint <- function(x, descending = TRUE) {
+  labels <- if(descending) 5:1 else 1:5 
   cutoff <- quantile(x, probs = c(0,0.2,0.4,0.6,0.8,1), na.rm = TRUE)
   if(sum(is.na(cutoff)) == 0) {
     as.numeric(as.character(cut(x,
                                 breaks = cutoff,
-                                labels = 5:1,
+                                labels = labels,
                                 include.lowest = TRUE)))
   } else rep(NA, length(x))
 }
