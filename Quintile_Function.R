bd3185fa112fc61d5709119eccbab1c68cf47043
Jeremy Bowyer <JeremyBowyer@gmail.com>
2017-10-02 12:36:00 -0400

    Updated quintile function to handle low N
    There was a bug where if the vector of values didn't have enough variability to create 6 unique breakpoints, the cut() function would throw an error. We are now passing the unique cutoff points to the cut() function, and replacing whatever the highest quintile ends up being with 5. So if there are only 4 unique cutoff points, resulting in 3 groups, the labels will be 1, 2, 5.

--- Quintile_Function.R
+++ Quintile_Function.R
@@ -1,12 +1,16 @@
 
 ## Function that transforms numeric vector into quintiles
 quint <- function(x, descending = TRUE) {
-  labels <- if(descending) 5:1 else 1:5 
-  cutoff <- quantile(x, probs = c(0,0.2,0.4,0.6,0.8,1), na.rm = TRUE)
+  
+  cutoff <- unique(quantile(x, probs = c(0,0.2,0.4,0.6,0.8,1), na.rm = TRUE))
+  labels <- if(descending) (length(cutoff) - 1):1 else 1:(length(cutoff) - 1)
+  labels <- replace(labels, labels == max(labels), 5)
+
   if(sum(is.na(cutoff)) == 0) {
     as.numeric(as.character(cut(x,
                                 breaks = cutoff,
                                 labels = labels,
                                 include.lowest = TRUE)))
   } else rep(NA, length(x))
+  
 }
